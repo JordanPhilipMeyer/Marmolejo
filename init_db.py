c3dd97a33dc8ff0aa81078e97af147658fae02ff
Jordan <meyer3jp@gmail.com>
2022-01-19 20:09:10 -0500

    fix: add database initializing script

---
+++ init_db.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sqlite3
+
+## USES THE TASK JOURNAL CSV AS A SIMPLE SCHEMA FOR YOUR DATABASE
+df = pd.read_csv("logs/task_journal_2021-12-26.csv")
+df.drop(df.index, inplace=True)
+conn = sqlite3.connect("eventDB.db")
+df.to_sql("task_journal", conn, if_exists="replace", index=False)
+conn.close()
